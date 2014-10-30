a8b1888986a50bd67b0dc5c4144ac38eea7e7ed5
Jason Kuhrt <jasonkuhrt@me.com>
2014-10-30 11:18:17 -0400

    Refactor cli into "get" and "update" module

---
+++ bin/index.js
@@ -0,0 +1,43 @@
+var I = require('immutable')
+var cli = require('commander')
+
+var dataReleaseTypes = I.Set.of('patch', 'minor', 'major')
+
+
+
+
+
+
+cli
+  .version(require('../package.json').version)
+  .option('-p, --patch', 'Make a patch release')
+  .option('-m, --minor', 'Make a minor release')
+  .option('-a, --major', 'Make a major release')
+  .option('-Y, --no-prompt', 'Disable confirmation prompt')
+  .option('-R, --no-registry', 'Disable registry publishing')
+
+cli.parse(process.argv)
+
+
+doRelease(findFirstKeyIntersect(dataReleaseTypes, cli))
+.then(console.log)
+.catch(console.error)
+
+
+
+
+
+
+function doRelease(releaseType){
+  get.releaseData(releaseType)
+  .then(function(releaseData){
+    return update.manifest(releaseData)
+    .then(update.localVCS.bind(null, releaseData))
+    .then(update.remoteVCS)
+  })
+  // .then(updateRegistry)
+}
+
+function findFirstKeyIntersect(set, obj){
+  return set.find(function(item){ return obj[item] })
+}
